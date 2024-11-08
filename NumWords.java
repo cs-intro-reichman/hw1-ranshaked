86352dc5184d3ac8fd36967bb171dc4d1ae3c8f3
Ran Shaked <ran.shaked@runi.ac.il>
2024-11-08 17:33:37 +0200

    numwords timeforamt finish

--- NumWords.java
+++ NumWords.java
@@ -1,6 +1,13 @@
 // Prints a given number using a hundreds, tens, and units notation.
 public class NumWords {
-	public static void main(String args[]) {
-	    // Replace this comment with your code
+	public static void main(String args[]) 
+	{
+	   int nW = Integer.parseInt(args[0]);
+	   int hundreds = (nW/100)%10;
+	   int tens = (nW/10)%10;
+	   int ones = nW%10;
+	   System.out.println(hundreds  + " hundreds, " +tens + " tens, and " +ones + " ones.");
+	 
+	   
 	}
 }
